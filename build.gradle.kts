9a20618c5d07d67d0ee0859b8386897053a32356
Vincent Joshua Tigas <vincentjoshuaet@gmail.com>
2020-08-11 20:10:26 +0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath(kotlin("gradle-plugin", "1.3.72"))
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.1.0-rc01")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.28.3-alpha")
         classpath("com.google.gms:google-services:4.3.3")
